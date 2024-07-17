3b25891621546dc7857167f9c216c389bf1c10fd
whirlxd <ninja21official@gmail.com>
2024-07-18 01:36:52 +0530

    remove light mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,6 @@ export default {
   daisyui: {
     themes: [
       {
-        light: {
-          primary: "#242f66",
-          secondary: "#7183e0",
-          accent: "#1534cb",
-          neutral: "#1a1d33",
-          "base-100": "#f1f2f8",
-        },
         dark: {
           primary: "#99a4db",
           secondary: "#1f318f",
